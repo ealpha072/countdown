995df88dadc4046995aa8008f3463713d8fd3d16
ealpha072 <ealpha072@gmail.com>
2021-05-04 21:01:03 +0300

    Uncommented redundant code and improved spacing

--- main.js
+++ main.js
@@ -1,133 +1,128 @@
 //dom elements
-    const eventName = document.querySelector('[name=event-name]');
-    const organizerName = document.querySelector('[name=Organizer-name]');
-    const day = document.querySelector('[name=day]');
-    const month = document.querySelector('[name=month]');
-    const year = document.querySelector('[name=year]');
-    const hour = document.querySelector('[name=hour]');
-    const minute = document.querySelector('[name=minute]');
-    const seconds = document.querySelector('[name=seconds]');
-    const button = document.querySelector("#start-timer");
-    const contentDiv = document.getElementById('content');
-    const timeHolder = document.getElementById('timer-holder');
-    const dayDiv = document.querySelector('.days');
-    const hrDiv = document.querySelector('.hour');
-    const minDiv = document.querySelector('.minute');
-    const secDiv = document.querySelector('.secs');
-    const infoDiv = document.querySelector('.info');
-    const buttonDiv = document.getElementById('button-holder')
+const eventName = document.querySelector('[name=event-name]');
+const organizerName = document.querySelector('[name=Organizer-name]');
+const day = document.querySelector('[name=day]');
+const month = document.querySelector('[name=month]');
+const year = document.querySelector('[name=year]');
+const hour = document.querySelector('[name=hour]');
+const minute = document.querySelector('[name=minute]');
+const seconds = document.querySelector('[name=seconds]');
+const button = document.querySelector("#start-timer");
+const contentDiv = document.getElementById('content');
+const timeHolder = document.getElementById('timer-holder');
+const dayDiv = document.querySelector('.days');
+const hrDiv = document.querySelector('.hour');
+const minDiv = document.querySelector('.minute');
+const secDiv = document.querySelector('.secs');
+const infoDiv = document.querySelector('.info');
+const buttonDiv = document.getElementById('button-holder')
 
 contentDiv.removeChild(timeHolder)
-//options to select
+    //options to select
 var yearOptions = [];
-    var currentYear=  new Date().getFullYear();
-    for(var i= currentYear;i<=currentYear+20;i++){
-    yearOptions.push({title:i,value:i})
-}
-
-var monthOptions=[];
-    for(var i = 1; i<=12;i++){
-    let monthObject ={
-        1:'January',2:"February",3:"March",
-        4:"April",5:"May",6:"June",
-        7:"July",8:"August",9:"September",
-        10:"October",11:"November",12:"December"
-    };
-    monthOptions.push({title:monthObject[i],value:i}) 
-}
-
-
+var monthOptions = [];
 var daysOption = [];
-    for(var i = 1;i<=31; i++){
-    daysOption.push({title:i,value:i})
+var hoursOptions = [];
+var secondsAndMinsOption = [];
+
+var currentYear = new Date().getFullYear();
+for (var i = currentYear; i <= currentYear + 20; i++) {
+    yearOptions.push({ title: i, value: i })
 }
 
 
-var hoursOptions = [];
-    for(var i= 0; i<=24;i++){
-    if(i === 0){
-        hoursOptions.push({title:"Midnight",value:i})
-    }else if(i === 12){
-        hoursOptions.push({title:i+" Noon",value:i})
-    }else if(i < 12){
-        hoursOptions.push({title:i+"am",value:i})
-    }else{
-        hoursOptions.push({title:i-12 +"pm",value:i})
+for (var i = 1; i <= 12; i++) {
+    let monthObject = {
+        1: 'January',
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+    };
+    monthOptions.push({ title: monthObject[i], value: i })
+}
+
+//number of months
+for (var i = 1; i <= 31; i++) {
+    daysOption.push({ title: i, value: i })
+}
+
+//time in hours
+for (var i = 0; i <= 24; i++) {
+    if (i === 0) {
+        hoursOptions.push({ title: "Midnight", value: i })
+    } else if (i === 12) {
+        hoursOptions.push({ title: i + " Noon", value: i })
+    } else if (i < 12) {
+        hoursOptions.push({ title: i + "am", value: i })
+    } else {
+        hoursOptions.push({ title: i - 12 + "pm", value: i })
     }
 }
 
-
-var secondsAndMinsOption =[]
-    for(var i = 0;i<=59;i++){
-    secondsAndMinsOption.push({title:i,value:i})
+//seconds and minutes
+for (var i = 0; i <= 59; i++) {
+    secondsAndMinsOption.push({ title: i, value: i })
 }
 
-function setUpOptions(selectElement,optionsArray){
-    optionsArray.forEach(function(option){
+//populate options 
+function setUpOptions(selectElement, optionsArray) {
+    optionsArray.forEach(function(option) {
         let options = document.createElement('option')
         options.value = option.value;
         options.text = option.title;
-        selectElement.add(options,null);
+        selectElement.add(options, null);
     })
 }
-    setUpOptions(day,daysOption);
-    setUpOptions(year,yearOptions);
-    setUpOptions(month,monthOptions);
-    setUpOptions(minute,secondsAndMinsOption);
-    setUpOptions(seconds, secondsAndMinsOption);
-    setUpOptions(hour,hoursOptions);
+setUpOptions(day, daysOption);
+setUpOptions(year, yearOptions);
+setUpOptions(month, monthOptions);
+setUpOptions(minute, secondsAndMinsOption);
+setUpOptions(seconds, secondsAndMinsOption);
+setUpOptions(hour, hoursOptions);
 
 //countdown function
-function countDown(){
-        contentDiv.appendChild(timeHolder)
-        var end = new Date(year.value,
-        (+month.value-1),day.value,hour.value,minute.value,seconds.value);
-        //console.log(end);
-        var myVar = setInterval(function(){
-            var endTime = new Date(end).getTime();
-            console.log(endTime)
-            var  currentTime= new Date().getTime();
-            var difference = end - currentTime;
-            if(difference <= 0 || eventName.value == ""){
-                alert("Fill in the event Name or check your date");
-                clearInterval(myVar);
-                eventName.removeAttribute("disabled",'');
-            }else{
-                var dayConversion = 1000*60*60*24;
-                var hrConversion = 60*60*1000
-                var minConversion = 60*1000;
-                var secConversion = 1000
-                var days = Math.floor(difference / dayConversion);
-                var hrs= Math.floor((difference % (dayConversion)) / (hrConversion))
-                var mins = Math.floor((difference %(hrConversion))/(minConversion))
-                var secs = Math.floor((difference%(minConversion))/(secConversion))
-                infoDiv.innerHTML = eventName.value
-                dayDiv.innerHTML = `${days}. `
-                hrDiv.innerHTML=`${hrs}. `
-                minDiv.innerHTML=`${mins}. `
-                secDiv.innerHTML=`${secs}`
-            }
-        },1000)
+function countDown() {
+    contentDiv.appendChild(timeHolder)
+    var end = new Date(year.value,
+        (+month.value - 1), day.value, hour.value, minute.value, seconds.value);
+    var myVar = setInterval(function() {
+        var endTime = new Date(end).getTime();
+        console.log(endTime)
+        var currentTime = new Date().getTime();
+        var difference = end - currentTime;
+        if (difference <= 0 || eventName.value == "") {
+            alert("Fill in the event Name or check your date");
+            clearInterval(myVar);
+            eventName.removeAttribute("disabled", '');
+        } else {
+            var dayConversion = 1000 * 60 * 60 * 24;
+            var hrConversion = 60 * 60 * 1000
+            var minConversion = 60 * 1000;
+            var secConversion = 1000
+            var days = Math.floor(difference / dayConversion);
+            var hrs = Math.floor((difference % (dayConversion)) / (hrConversion))
+            var mins = Math.floor((difference % (hrConversion)) / (minConversion))
+            var secs = Math.floor((difference % (minConversion)) / (secConversion))
+            infoDiv.innerHTML = eventName.value
+            dayDiv.innerHTML = `${days}. `
+            hrDiv.innerHTML = `${hrs}. `
+            minDiv.innerHTML = `${mins}. `
+            secDiv.innerHTML = `${secs}`
+        }
+    }, 1000)
 }
 
-button.addEventListener('click',(e)=>{
+button.addEventListener('click', (e) => {
     e.preventDefault();
     countDown();
-    //setTimeout(function(){eventName.value =''},3000);
-    //organizerName.value ='';
-
-    //start below
-    eventName.setAttribute('disabled','');
-    button.setAttribute('disabled','');
-    /*const button2 = document.createElement('button');
-    button2.textContent = 'New Event';
-    buttonDiv.appendChild(button2)
-    button2.addEventListener('click',(e)=>{
-        e.preventDefault();
-        eventName.removeAttribute("disabled",'');
-        eventName.value='';
-        button.removeAttribute('disabled','');
-        contentDiv.removeChild(timeHolder)
-    })*/
-    
+    eventName.setAttribute('disabled', '');
+    button.setAttribute('disabled', '');
 });
